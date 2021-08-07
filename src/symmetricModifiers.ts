84d3ab758e5cd805cfb5a2e42dd1bcd6b963ff9d
David Little <david.frank.little@gmail.com>
2021-08-06 23:30:41 -0400

    fix: bug in symmetric motions across lines

--- src/symmetricModifiers.ts
+++ src/symmetricModifiers.ts
@@ -13,6 +13,26 @@ interface InsertAroundArgs{
     before: string,
     after: string
 }
+
+function wrappedTranslate(x: vscode.Position, doc: vscode.TextDocument, val: number){
+    if(val < 0){
+        let result = x
+        while(result.character + val < 0){
+            val += 1;
+            result = result.translate(-1, 0);
+            result = result.translate(0, doc.lineAt(result).range.end.character)
+        }
+        return result.translate(0, val);
+    }else{
+        let result = x;
+        while(result.character + val > doc.lineAt(result).range.end.character){
+            val -= 1;
+            result = new vscode.Position(result.line+1, 0)
+        }
+        return result.translate(0, val);
+    }
+}
+
 async function insertAround(args: InsertAroundArgs){
     let editor = vscode.window.activeTextEditor;
     if(editor){
@@ -31,11 +51,11 @@ async function insertAround(args: InsertAroundArgs){
             if(!sel.isReversed){
                 return new vscode.Selection(
                     sel.anchor,
-                    sel.active.translate(0, -1),
+                    wrappedTranslate(sel.active, ed.document, -1),
                 );
             }else{
                 return new vscode.Selection(
-                    sel.anchor.translate(0, -1),
+                    wrappedTranslate(sel.anchor, ed.document, -1),
                     sel.active
                 );
             }
@@ -50,27 +70,29 @@ function deleteAround(args: {count?: number }){
         editor.edit(builder => {
             for(const sel of ed.selections){
                 builder.delete(new vscode.Range(
-                    sel.start.translate(0, -(args.count || 1)), 
+                    wrappedTranslate(sel.start, ed.document, -(args.count || 1)), 
                     sel.start)
                 );
                 builder.delete(new vscode.Range(
                     sel.end, 
-                    sel.end.translate(0, (args.count || 1)))
+                    wrappedTranslate(sel.end, ed.document, (args.count || 1)))
                 );
             }
         });
     }
 }
 
+
 function adjustSelections(args: {dir: string, count: number}){
     let editor = vscode.window.activeTextEditor;
     let step = args.dir === 'forward' ? (args.count || 1) : -(args.count || 1);
     if(editor){
+        let ed = editor;
         editor.selections = editor.selections.map(sel => {
             let sign = sel.isReversed ? -1 : 1;
             return new vscode.Selection(
-                sel.anchor.translate(0, -sign*step),
-                sel.active.translate(0,  sign*step)
+                wrappedTranslate(sel.anchor, ed.document, -sign*step),
+                wrappedTranslate(sel.active, ed.document,  sign*step)
             );
         });
     }
